1b658db4a054c12aeea1ffad1051db1200d27c21
Michael Ray <michaelburtonray@gmail.com>
2012-05-02 21:16:38 -0400

    set up private methods and variables

--- js/magic.rows.js
+++ js/magic.rows.js
@@ -1,34 +1,82 @@
-;(function($, undefined) {
-
+;(function($) {
+  var _$this = $(this);
 
 
   $.fn.magicRows = function ( options ) {
+    // support multiple elements
+    if (this.length > 1){
+      this.each(function() { $(this).magicRows(options) });
+      return this;
+    }
 
+    // private variables
+    var images_array = [];
+    var row = [];
+
+    // settings
     var settings = $.extend({
       max_height: 225
     }, options);
 
-    return this.each(function(){
-      var $this = $(this);
-      var $children = $this.children();
 
-      var height = 0;
+
+    // private methods
+
+
+
+
+    // public methods
+    this.init = function() {
+
+      console.log(this);
+
+      var $children = $(this).children();
+
       $children.each(function(){
         var $this = $(this);
-        height = height + $this.height();
 
-        $this.height('100');
-
-        console.log(height);
+        images_array.push({
+          aspect_ratio: $this.width() / $this.height(),
+          height: $this.height(),
+          index: $this.index(),
+          width: $this.width()
+        });
 
       });
 
+      console.log(['images_array',images_array]);
+
+      return this;
+    }
+
+    this.trigger = function() {
+      console.log(this.width());
+      // Row queue
+
+      // while(images_array.length > 0) {
+
+      //   var image = images_array.shift();
+
+      //   row.push(image);
+
+      //   var denominator
+
+      //   for(var i = 0;i < row.length;i++) {
+      //     denominator += row[i];
+      //   }
+
+      //   // var height = 
 
 
-    });
+      // }
+
+      return this;
+    }
+
+    return this.init().trigger();
 
   };
 
 
 
-})( jQuery );
+})(jQuery);
